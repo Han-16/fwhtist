ab08c7de5347cf094e046ff701397c7a9bf1370f
Han-16 <haanbk16@gmail.com>
2025-09-21 16:22:52 +0900

    feat: add another base case

---
+++ gnark_circuit/poseidonScalar/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/consensys/gnark-crypto/ecc"
+	"github.com/consensys/gnark/frontend"
+	"github.com/consensys/gnark/test"
+)
+
+type PoseidonCircuit struct {
+}
